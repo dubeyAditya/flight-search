fbd7dc9332c95e7fb055cf4bc9f44a730e6e27a5
Aditya Dubey <sinister.dubey@gmail.com>
2019-04-06 23:16:43 +0530

    removed serviceworker

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import mixins from './mixins/mixin'
-import './registerServiceWorker'
+//import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
